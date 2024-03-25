feb6cc6890a5e7f8bfdacd2500341e4da7aa66a1
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:25:21 -0400

    atividade30 adicionada

---
+++ atividade30.js
@@ -0,0 +1,13 @@
+/*
+Questão 30: Como você pode verificar se uma variável é definida em 
+JavaScript
+*/
+
+let variavel = 1;
+
+if(typeof variavel === "undefined") {
+    console.log("variavel não definida");
+
+} else {
+    console.log("variavel definida"); 
+}
