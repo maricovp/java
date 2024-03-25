a0f1db62542a61e6d30545f88fc75ae565d1cc27
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:18:50 -0400

    atividade3 adicionada

---
+++ atividade3.js
@@ -0,0 +1,12 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript?
+*/
+
+let parte1 = "AAAAAA"
+let parte2 = "MARAN"
+let resultado;
+
+resultado = parte1 +" "+ parte2;
+console.log(resultado);
+
+
