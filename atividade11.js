1015434fedfc12f2c411b0bb19c7dc8114477c0b
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:22:32 -0400

    atividade11 adicionada

---
+++ atividade11.js
@@ -0,0 +1,5 @@
+/*Questão 11: Qual é a diferença entre let, const e var em JavaScript?
+const= uma variavel constante que smpre vai estar em uso.
+let= usado pra identificar os elementos.
+var= var usado para dar valor aos elementos.
+*/
