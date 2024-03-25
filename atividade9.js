eed0f3114a75dd74c4375fd32d07e17e5f4f7f34
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:19:50 -0400

    atividade9 adicionada

---
+++ atividade9.js
@@ -0,0 +1,10 @@
+/*
+Questão 9: Como você pode adicionar um novo elemento ao final de um array 
+em JavaScript?
+*/
+
+var alunos = ["a", "b", "c", "ola alunos"];
+
+alunos.push("ola alunos");
+
+console.log(alunos);
