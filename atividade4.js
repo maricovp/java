fa9650f4ec850a5f4aad9ca21f766f8a17032a29
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:18:58 -0400

    atividade4 adicionada

---
+++ atividade4.js
@@ -0,0 +1,18 @@
+/*
+Qual é a diferença entre == e === em JavaScript?
+
+Resposta: os operadores == e === tem como função comparar valores
+
+Comparações retornam um booleano, true ou false
+
+Ou seja, quando os utilizamos estamos procurando validar algo
+
+A grande diferença ocorre no ===, onde o tipo de dado é comparado também
+*/
+
+let num1 = 1;
+let num2 = 1;
+
+console.log(num1 == num2);
+console.log(typeof num1);
+console.log(typeof num2);
