0b1e42f7218b874222a0e65f7f8dd2eaa7d883a4
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:25:01 -0400

    atividade28 adicionada

---
+++ atividade29.js
@@ -0,0 +1,9 @@
+/*
+Questão 29: Como você pode converter um número em uma string em 
+JavaScript?
+*/
+
+let stringNumero = 42;
+console.log(typeof stringNumero);
+let cs = stringNumero.toString();
+console.log(typeof cs); // 42
