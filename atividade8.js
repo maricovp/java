c617cec39777dbbf4c4bf2c0e22b8a2b0f148e0e
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:19:41 -0400

    atividade8 adicionada

---
+++ atividade8.js
@@ -0,0 +1,14 @@
+/*
+Questão 8: Como você pode acessar o último elemento de um array em 
+JavaScript?
+*/
+
+let alunos = ["a", "b", "c", "ola alunos"];
+
+let contagem = alunos.length -1;
+
+let elemento = alunos[contagem];
+
+console.log(elemento)
+
+
