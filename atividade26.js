c2cfe7d1d3da9cb466c49397c92c771f816a6be0
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:24:40 -0400

    atividade26 adicionada

---
+++ atividade26.js
@@ -0,0 +1,10 @@
+/*Questão 26: Como você pode usar o operador ternário em JavaScript?
+*/
+let idadeUsuario = 40;
+
+let textoConsole =
+    idadeUsuario >= 65
+        ? "sua aposentadoria esta aprovada."
+        : "Apenas pessoas com 65 anos ou mais possuem direito à aposentadoria.";
+console.log(textoConsole);
+ 
