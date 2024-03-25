3da48049b50e67e0d77d8e8959b5dd477f07bb45
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:23:14 -0400

    atividade18 adicionada

---
+++ atividade18.js
@@ -0,0 +1,11 @@
+/*
+Questão 18: Como você pode acessar o valor de uma propriedade de um 
+objeto em JavaScript?
+*/
+
+const obj = {}
+console.log(obj) 
+const pessoa = {
+    nome: "Marcos"
+}
+console.log(pessoa.nome)//para acessar o valor de uma propriedade de um objeto adicione o . e o nome do objeto dentro do parenteses de console.log()
