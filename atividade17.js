4a31048f2b747a900562dbb9ce1c105125e23db3
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:23:08 -0400

    atividade17 adicionada

---
+++ atividade17.js
@@ -0,0 +1,11 @@
+/*
+Questão 17: Como você pode adicionar uma propriedade a um objeto em 
+JavaScript?
+*/
+
+const obj = {}
+console.log(obj) 
+const pessoa = {
+    nome: "Marcos"
+}
+console.log(pessoa) // Nome: marcos
