35d0ec8b7a797ff4bc0dcfce213e3513f54dcac8
maricovp <marcos2015vinicuis@gmail.com>
2024-03-25 09:19:58 -0400

    atividade10 adicionada

---
+++ atividade10.js
@@ -0,0 +1,13 @@
+/*Questão 10: Como você pode remover o último elemento de um array em JavaScript?*/
+/*
+Questão 10: Como você pode remover o último elemento de um array em 
+JavaScript?
+*/
+
+let alunos = ["a", "b", "c", "aloalunos",];
+
+console.log(alunos);
+
+let alunosRemovido = alunos.pop();
+
+console.log(alunosRemovido);
